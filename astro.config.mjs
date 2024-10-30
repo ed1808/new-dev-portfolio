ad8c7bcdd9686b4256d298df3ab08db76a00fc41
Edward Rodríguez <erodriguez@comunike.com>
2024-10-30 18:11:18 -0500

    Update adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig, envField } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
-import cloudflare from '@astrojs/cloudflare';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
@@ -18,5 +18,5 @@ export default defineConfig({
     }
   },
 
-  adapter: cloudflare()
+  adapter: vercel(),
 });
